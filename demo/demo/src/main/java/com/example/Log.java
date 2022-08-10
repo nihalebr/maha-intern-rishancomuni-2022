b4f6697507d3ee27e456de506bdc0b1f852f2a50
jsrkamal <90234628+jsrkamal2002@users.noreply.github.com>
2022-08-10 12:07:14 +0530

    2nd task finished

--- demo/demo/src/main/java/com/example/Log.java
+++ demo/demo/src/main/java/com/example/Log.java
@@ -1,4 +1,9 @@
+// (ip,id) - url
+// task to do - DATE: 10.8.22
+
 package com.example;
+
+
 
 /**
  * Hello world!
@@ -12,34 +17,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.sql.*;
   
-public class App {
+public class Log {
 	
 	  
 	  public static void DateExtraction(String regex) throws SQLException,ClassNotFoundException {
 	      Pattern pattern = Pattern.compile(regex);
 	      String line;
-        Class.forName("org.postgresql.Driver");
-        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost/postgres","postgres","Kamal2002*");
-        Statement stmt = con.createStatement();
-        String sql = "CREATE TABLE IF NOT EXISTS LOG( RequestedTime TEXT,"
-        + "RequestedTime_in_ms TEXT,"
-        + "ClientIp TEXT,"
-        + "LocalPort TEXT,"
-        + "RemotePort TEXT,"
-        + "ServerHostName TEXT,"
-        + "URL TEXT,"
-        + "RequestLine TEXT,"
-        + "Content_Type_incoming TEXT,"
-        + "Content_Type_outgoing TEXT,"
-        + "Referer TEXT,"
-        + "Status TEXT,"
-        + "TimeTaken_in_ms TEXT,"
-        + "Size TEXT,"
-        + "SessionId TEXT,"
-        + "Protocol TEXT,"
-        + "UserAgent TEXT);";
-        stmt.execute(sql);
-	
+       
 	      try {
 	        //Reading the data present in a given file from FileReader using the BufferedReader.
 	        //Here, the location is the path of the file. Kindly change the location to the correct location of the required file in your system. 
@@ -59,14 +43,7 @@ public class App {
 	                // System.out.println(matchPattern.group());
                     sList.add(matchPattern.group());
 	              }
-                  sql = "INSERT INTO LOG VALUES(";
-                  for(int j =0;j<sList.size();j++) {
-                      sql = sql + "'"+  sList.get(j).substring(1, sList.get(j).length()-1) + "',";
-                  }
-                    sql = sql.substring(0, sql.length()-1) + ");";
-                    System.out.println(sql);
-                    stmt.execute(sql);
-                  System.out.println(sql);
+                System.out.println("("+sList.get(2) + ","+ sList.get(14)+") - "+sList.get(6));
                 
 	         }
             }
