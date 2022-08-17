90f8fc00aa033c4d76df8ed74f791a3520aa3afa
EbrahimNihal <ebrahimsickkanderp.ug20.cs@francisxavier.ac.in>
2022-08-17 22:19:51 +0530

    made some change

--- demo/demo/src/main/java/com/example/Log.java
+++ demo/demo/src/main/java/com/example/Log.java
@@ -3,62 +3,58 @@
 
 package com.example;
 
-
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
 /**
  * Hello world!
  *
  */
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.sql.*;
-  
+
 public class Log {
-	
-	  
-	  public static void DateExtraction(String regex) throws SQLException,ClassNotFoundException {
-	      Pattern pattern = Pattern.compile(regex);
-	      String line;
-       
-	      try {
-	        //Reading the data present in a given file from FileReader using the BufferedReader.
-	        //Here, the location is the path of the file. Kindly change the location to the correct location of the required file in your system. 
-	        String location = "C:\\Users\\jsrk\\Desktop\\maha-intern-rishancomuni-2022\\log.txt"; 
-	        BufferedReader lineReader = new BufferedReader(new FileReader(location));
-	        //Reading the contents of the file line by line
 
-	        while ((line = lineReader.readLine()) != null) {
-	             //Matching each line with the regular expression pattern to check if there are any dates present.
-	             Matcher matchPattern = pattern.matcher(line);
-	             //Enters the while loop only if there is a match found.
-                 int count = 0;
-                 ArrayList<String> sList = new ArrayList<String>();
-	             while(matchPattern.find()) {
-                    count++;
-	                 //Printing all the dates present in that line. 
-	                // System.out.println(matchPattern.group());
-                    sList.add(matchPattern.group());
-	              }
-                System.out.println("("+sList.get(2) + ","+ sList.get(14)+") - "+sList.get(6));
-                
-	         }
-            }
-	       catch (IOException e) {
-	         // Occurs whenever an input or output operation is failed or interpreted. 
-	         //For example, trying to read from a file that does not exist. 
-	         System.out.println("Error: " + e.getMessage());
-	       }
-	   }
-	   
-	   public static void main(String[] args) throws SQLException,ClassNotFoundException {
-	         //Regular Expression to recognize all the dates of the format 'dd/mm/yyyy' or 'dd-mm-yyyy'.       
-	         String regex = "\"[^\"]*\"";
-	         DateExtraction(regex);  
+  public static void DateExtraction(String regex)
+    throws SQLException, ClassNotFoundException {
+    Pattern pattern = Pattern.compile(regex);
+    String line;
 
+    try {
+      //Reading the data present in a given file from FileReader using the BufferedReader.
+      //Here, the location is the path of the file. Kindly change the location to the correct location of the required file in your system.
+      String location =
+        "C:\\Users\\jsrk\\Desktop\\maha-intern-rishancomuni-2022\\log.txt";
+      BufferedReader lineReader = new BufferedReader(new FileReader(location));
+      //Reading the contents of the file line by line
+
+      while ((line = lineReader.readLine()) != null) {
+        //Matching each line with the regular expression pattern to check if there are any dates present.
+        Matcher matchPattern = pattern.matcher(line);
+        ArrayList<String> sList = new ArrayList<String>();
+        while (matchPattern.find()) {
+          //Printing all the dates present in that line.
+          // System.out.println(matchPattern.group());
+          sList.add(matchPattern.group());
+        }
+        System.out.println(
+          "(" + sList.get(2) + "," + sList.get(14) + ") - " + sList.get(6)
+        );
+      }
+      lineReader.close();
+    } catch (IOException e) {
+      // Occurs whenever an input or output operation is failed or interpreted.
+      //For example, trying to read from a file that does not exist.
+      System.out.println("Error: " + e.getMessage());
     }
-	
+  }
+
+  public static void main(String[] args)
+    throws SQLException, ClassNotFoundException {
+    //Regular Expression to recognize all the dates of the format 'dd/mm/yyyy' or 'dd-mm-yyyy'.
+    String regex = "\"[^\"]*\"";
+    DateExtraction(regex);
+  }
 }
--- demo/demo/src/main/java/com/example/log4_try.java
+++ demo/demo/src/main/java/com/example/log4_try.java
@@ -1,51 +1,87 @@
 package com.example;
+
 import java.sql.*;
 
 public class log4_try {
-    public static void MaxTime() throws SQLException,ClassNotFoundException {
-        Class.forName("org.postgresql.Driver");
-        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost/postgres","postgres","Kamal2002*");
-        Statement stmt = con.createStatement();
-        String sql = "select distinct clientip,sessionid from log group by clientip,sessionid;";
-        ResultSet rs = stmt.executeQuery(sql);
-        while (rs.next()) {
-            System.out.println(rs.getString(1) + " " + rs.getString(2));
-        }
-        String qryHighTimeUrl = "select clientip,sessionid,url,timetaken_in_ms from log where clientip='"+rs.getString(1)+"' and sessionid='"+rs.getString(2)+"' order by timetaken_in_ms desc;";
-        System.out.println(qryHighTimeUrl);
-        ResultSet rs1 = stmt.executeQuery(qryHighTimeUrl);
-        while (rs1.next()) {
-            // System.out.println(rs.getString(1) + " " + rs.getString(2));
-            System.out.println(rs1.getString(1) + " " + rs1.getString(2)+ " " + rs1.getString(3)+ " " + rs1.getString(4));
 
-        }
-            rs.last();
-            int rowCount = rs.getRow();
-            rs.first();
-            String[] ip= new String[rowCount];
-            String[] id = new String[rowCount];
-            int counter=0;
-            while(rs.next()){
-                ip[counter]=rs.getString("clientip");
-                id[counter]=rs.getString("sessionid");
-                counter++;
-            }
-            for (int i = 0; i < ip.length; i++) {
-                String qryHighTimeString = "select clientip,sessionid,url,timetaken_in_ms from log where clientip='"+ip[i]+"' and sessionid='"+ id[i] +"' order by timetaken_in_ms desc;";
-                ResultSet rs2 = stmt.executeQuery(qryHighTimeString);
-                while (rs2.next()) {
-                    // System.out.println(rs.getString(1) + " " + rs.getString(2));
-                    System.out.println(rs2.getString(1) + " " + rs2.getString(2)+ " " + rs2.getString(3)+ " " + rs2.getString(4));
-                }
-            }
-            // for (int i =0 ; i < ip.length; i++) {
-            //     System.out.println(ip[i]);
-            // }
-
-
+  public static void MaxTime() throws SQLException, ClassNotFoundException {
+    Class.forName("org.postgresql.Driver");
+    Connection con = DriverManager.getConnection(
+      "jdbc:postgresql://localhost/postgres",
+      "postgres",
+      "!sHuman"
+    );
+    Statement stmt = con.createStatement(
+      ResultSet.TYPE_SCROLL_INSENSITIVE,
+      ResultSet.CONCUR_READ_ONLY
+    );
+    String sql =
+      "select distinct clientip,sessionid from log group by clientip,sessionid;";
+    ResultSet rs = stmt.executeQuery(sql);
+    while (rs.next()) {
+      System.out.println(rs.getString(1) + " " + rs.getString(2));
     }
-
-    public static void main(String[] args) throws SQLException,ClassNotFoundException {
-        MaxTime();
+    rs.first();
+    String qryHighTimeUrl =
+      "select clientip,sessionid,url,timetaken_in_ms from log where clientip='" +
+      rs.getString(1) +
+      "' and sessionid='" +
+      rs.getString(2) +
+      "' order by timetaken_in_ms desc;";
+    System.out.println(qryHighTimeUrl);
+    ResultSet rs1 = stmt.executeQuery(qryHighTimeUrl);
+    while (rs1.next()) {
+      // System.out.println(rs.getString(1) + " " + rs.getString(2));
+      System.out.println(
+        rs1.getString(1) +
+        " " +
+        rs1.getString(2) +
+        " " +
+        rs1.getString(3) +
+        " " +
+        rs1.getString(4)
+      );
     }
+    rs.last();
+    int rowCount = rs.getRow();
+    rs.first();
+    String[] ip = new String[rowCount];
+    String[] id = new String[rowCount];
+    int counter = 0;
+    while (rs.next()) {
+      ip[counter] = rs.getString("clientip");
+      id[counter] = rs.getString("sessionid");
+      counter++;
+    }
+    for (int i = 0; i < ip.length; i++) {
+      String qryHighTimeString =
+        "select clientip,sessionid,url,timetaken_in_ms from log where clientip='" +
+        ip[i] +
+        "' and sessionid='" +
+        id[i] +
+        "' order by timetaken_in_ms desc;";
+      ResultSet rs2 = stmt.executeQuery(qryHighTimeString);
+      while (rs2.next()) {
+        // System.out.println(rs.getString(1) + " " + rs.getString(2));
+        System.out.println(
+          rs2.getString(1) +
+          " " +
+          rs2.getString(2) +
+          " " +
+          rs2.getString(3) +
+          " " +
+          rs2.getString(4)
+        );
+      }
+    }
+    // for (int i =0 ; i < ip.length; i++) {
+    //     System.out.println(ip[i]);
+    // }
+
+  }
+
+  public static void main(String[] args)
+    throws SQLException, ClassNotFoundException {
+    MaxTime();
+  }
 }
